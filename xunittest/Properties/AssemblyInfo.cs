3bea757379ab4567acb5ee4b24f96aaa992889d8
Herrera Garay <KHerrera@bnb.com.bo>
2020-01-24 16:30:51 -0400

    primer commit

---
+++ xunittest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("xunittest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("xunittest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0078df9d-cf2b-42ee-b8eb-9d4f5a8c40fd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ xunittest/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using System;
+
+namespace xunittest
+{
+   
+    public class UnitTest1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Console.Write("----------------------------------------------------xunit------------------");
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Console.Write("-------------------------xunit2------------------");
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
